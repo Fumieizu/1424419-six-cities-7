9fa9e1441436ffa35e588f2fa569a132b5f139ed
Fumieizu <71212206+Fumieizu@users.noreply.github.com>
2021-06-04 22:17:21 +0500

    Начинаем программировать

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,7 +1,18 @@
 import React from 'react';
+import Main from '../main/main';
+import PropTypes from 'prop-types';
 
-function App() {
-  return <p>Hello, world!</p>;
+function App({placeCount}) {
+
+  return (
+    <Main
+      placeCount = {placeCount}
+    />
+  );
 }
+
+App.propTypes = {
+  placeCount: PropTypes.number.isRequired,
+};
 
 export default App;
--- project/src/index.js
+++ project/src/index.js
@@ -2,8 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const MainInfo = {
+  PLACE_COUNT: 200,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      placeCount = {MainInfo.PLACE_COUNT}
+    />
   </React.StrictMode>,
-  document.getElementById('root'));
+
+  document.getElementById('root'),
+);
