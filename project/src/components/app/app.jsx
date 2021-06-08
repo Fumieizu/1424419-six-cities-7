13fe7af7f234037e8f8ab02058de124f5490e416
Fumieizu <71212206+Fumieizu@users.noreply.github.com>
2021-06-09 00:51:02 +0500

    Маршрут по тайным тропам

--- project/src/components/app/app.jsx
+++ project/src/components/app/app.jsx
@@ -1,13 +1,34 @@
 import React from 'react';
 import Main from '../main/main';
 import PropTypes from 'prop-types';
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
+import Favorites from '../favorites/favorites';
+import Room from '../room/room';
+import SignIn from '../sign-in/sign-in';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
+
+const OFFER_ID = 'id';
 
 function App({placeCount}) {
-
   return (
-    <Main
-      placeCount = {placeCount}
-    />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.ROOT}>
+          <Main placeCount={placeCount} />
+        </Route>
+        <Route exact path={AppRoute.FAVORITES}>
+          <Favorites/>
+        </Route>
+        <Route exect path={AppRoute.LOGIN}>
+          <SignIn/>
+        </Route>
+        <Route exact path={`${AppRoute.OFFER}/:${OFFER_ID}`} component={Room}/>
+        <Route>
+          <NotFoundScreen/>
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
